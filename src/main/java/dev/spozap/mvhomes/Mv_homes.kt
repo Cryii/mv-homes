f8be5ec0910ae752a0a6b14b0c60d5f28f899517
spozap <spozapdev@gmail.com>
2023-11-30 19:38:22 +0100

    first commit

---
+++ src/main/java/dev/spozap/mvhomes/Mv_homes.kt
@@ -0,0 +1,13 @@
+package dev.spozap.mvhomes
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Mv_homes : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
